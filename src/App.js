d217171eceef0c4aa0e377c5e60f96d328aa7793
unknown <ray33791@gmail.com>
2022-10-14 15:04:23 +0300

    runaway button

--- src/App.js
+++ src/App.js
@@ -5,20 +5,43 @@ import './App.css';
 
 function App() {
 
-  const [color, setColor] = React.useState('')
+  const [color, setColor] = React.useState('');
+  const [top, setTop] = React.useState('');
+  const [left, setLeft] = React.useState('');
+
+  const arr = ['банан', 'яблоко', 'груша'];
+  const maxIndex = arr.length - 1;
+  const [index, setIndex] = React.useState(0);
 
   function getRandomColor() {
     for (var i = 0; i < 6; i++) {
      setColor(`#${Math.floor(Math.random()*16777215).toString(16)}`);
    }
-};
+}
+
+function runAway() {
+   setTop(`${Math.floor(Math.random()*1000)}px`);
+   setLeft(`${Math.floor(Math.random()*1000)}px`);
+}
+
+const getRandom = () => {
+  setIndex(Math.ceil(Math.random() * maxIndex));
+
+}
+
+const updateStyles = () => {
+        getRandomColor();
+        runAway();
+        getRandom();
+      };
+
   
 
   return (
     <div>
       <button 
-    style ={{backgroundColor: color, width: '80px', height: '80px', cursor: 'pointer', borderRadius: '100%'}}
-    onClick={getRandomColor}>Nigger</button>
+    style ={{backgroundColor: color, width: '80px', height: '80px', cursor: 'pointer', borderRadius: '100%', position: 'absolute', top: top, left: left}}
+    onClick={updateStyles}></button>
     </div>
   )};
 
